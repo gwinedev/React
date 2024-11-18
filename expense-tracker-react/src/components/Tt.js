dee3ad398dc0ae0abd201e803e4e0d9b308bc590
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-11-18 12:52:22 +0100

    run test on Hooks successfully

--- expense-tracker-react/src/components/Tt.js
+++ expense-tracker-react/src/components/Tt.js
@@ -6,6 +6,19 @@ export const Tt = () => {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>You have eaten {candies} candies!</h1>
+
+      <button
+        onClick={() => setCandies(candies + 1)}
+        style={{
+            padding: '10px 20px',
+            fontSize: '16px',
+            cursor: 'pointer',
+            backgroundColor: 'lightblue',
+            border: 'none',
+            borderRadius: '5px'
+        }}>
+            Eat a Candy
+      </button>
     </div>
   )
 }
