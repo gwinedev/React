6e4b3d3d691a926cd51738fd3ef387e711069c1a
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-11-20 16:40:12 +0100

    implement multiple selection

--- Assorted/projects/src/components/accordion/index.jsx
+++ Assorted/projects/src/components/accordion/index.jsx
@@ -7,17 +7,40 @@ import './style.css';
 
 export default function Accordion() {
     const [selected, setSelected] = useState(null);
+    const [enableMultipleSelection, setEnableMultipleSelection] = useState(false);
+
+    const [multiple, setMultiple] = useState([]);
 
     function handleSingleSelection(id){
         setSelected(id === selected ? null : id);
     }
 
+    function handleMultipleSelection(id) {
+        let copyMultiple = [...multiple];
+        const IndexCurrentId = copyMultiple.indexOf(id);
+
+        if(IndexCurrentId === -1)
+            copyMultiple.push(id)
+        else   
+            copyMultiple.splice(IndexCurrentId, 1)
+        setMultiple(copyMultiple);
+    }
+    console.log(selected,multiple);
+
+
     return <div className="wrapper">
+        <button onClick={() => setEnableMultipleSelection(!enableMultipleSelection)}>Enable Multiple Selection</button>
         <div className='accordion'>
             {
                 data && data.length > 0 ? data.map(dataItem=>
                 <div className='item'>
-                    <div onClick={() =>handleSingleSelection(dataItem.id)} className='title'>
+                    <div onClick={
+                        enableMultipleSelection
+                            ? () => handleSingleSelection(dataItem.id)
+                            : () => handleMultipleSelection(dataItem.id)
+                    }
+                        className='title'
+                >
                         <h3>{dataItem.question}</h3>
                         <span>+</span>
                     </div>
