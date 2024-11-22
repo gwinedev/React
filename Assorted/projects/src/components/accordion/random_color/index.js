f25edba77307221ccb3f14520dea9a3043b48cdc
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-11-22 12:06:45 +0100

    start a hex, rgb project

--- Assorted/projects/src/components/accordion/random_color/index.js
+++ Assorted/projects/src/components/accordion/random_color/index.js
@@ -5,10 +5,33 @@ export default function RandomColor() {
 
     const [typeOfColor, setTypeOfColor] = useState('hex');
     const [color, setColor] = useState('#000000');
+    // const [colorName, setColorName] = useState('#000000')
 
-    function handleCreateRandomColor(){
-
+    function randomColorUtility(length) {
+        return Math.floor(Math.random() * length)
     }
+
+    function handleCreateHexRandomColor() {
+        const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+        let hexColor = "#";
+
+        for (let i = 0; i < 6; i++) {
+            hexColor += hex[randomColorUtility(hex.length)];
+        }
+
+        setColor(hexColor);
+        // setColorName(hexColor);
+    }
+
+    function handleCreateRgbRandomColor() {
+        const r = randomColorUtility(256);
+        const g = randomColorUtility(256);
+        const b = randomColorUtility(256);
+
+        setColor(`rgb${r}, ${g}, ${b}`);
+    }
+
+
     return <div style={{
         width: "100vw",
         height: "100vh",
@@ -21,7 +44,8 @@ export default function RandomColor() {
 
     }}
     >
-        <button onClick={() ={typeOfColor === 'hex' ? handleCreateHexRandomColor : handleCreateRgbRandomColor()}>Generate Random Color</button>
+        <span style={{color:"white", display:"flex", fontSize:"30px"}}>{color}</span>
+        <button onClick={typeOfColor === 'hex' ? handleCreateHexRandomColor : handleCreateRgbRandomColor()}>Generate Random Color</button>
         <button onClick={() => setTypeOfColor('hex')}>HEX Color</button>
         <button onClick={() => setTypeOfColor('rgb')}>RGB Color</button>
     </div>
