ab61fb9d557125be5bd902bf1ba2d32773385fbf
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-11-22 14:42:09 +0100

    star rating app

--- Assorted/projects/src/App.js
+++ Assorted/projects/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Accordion from './components/accordion';
-import RandomColor from './components/accordion/random_color';
+// import Accordion from './components/accordion';
+// import RandomColor from './components/random_color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordion /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      < StarRating starCount={10} />
     </div>
   );
 }
---
+++ Assorted/projects/src/components/star-rating/index.js
@@ -0,0 +1,37 @@
+import { FaStar } from 'react-icons/fa';
+import React, { useState } from 'react';
+
+export default function StarRating({ starCount = 5 }) {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick(index) {
+        setRating(index);
+    }
+
+    function handleMouseEnter(index) {
+        setHover(index);
+
+    }
+
+    function handleMouseLeave(index) {
+        console.log(index);
+
+    }
+    return <div className="star-rating">
+        {
+            [...Array(starCount)].map((_, index) => {
+                index += 1
+
+                return <FaStar
+                    key={index}
+                    className={index <= (hover || rating) ? 'active' : 'inactive'}
+                    onClick={() => handleClick(index)}
+                    onMouseMove={() => handleMouseEnter(index)}
+                    onMouseLeave={() => handleMouseLeave(index)}
+                    size={40}
+                />
+            })
+        }
+    </div>
+}
