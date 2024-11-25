66554f57673848b0f387bdbeba57a8de91772e50
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-11-25 08:37:38 +0100

    set api fetcher

--- Assorted/projects/src/components/image-slider/index.js
+++ Assorted/projects/src/components/image-slider/index.js
@@ -6,10 +6,17 @@ const ImageSlider = ({url, limit}) => {
     const [Images, setImages] = useState([]);
     const [currentSlide, setCurrentSlide] = useState(0);
     const [errorMsg, setErrorMsg] = useState(null);
+    const [loading, setLoading] = useState(null);
 
     async function fetchImages(getUrl) {
         try{
             const response = await fetch(getUrl);
+            const data = await response.json();
+
+            if(data) {
+                setImages(data);
+                setLoading(false);
+            }
         }catch(e){
             setErrorMsg(e.message);
         }
