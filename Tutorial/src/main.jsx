58f2c7c0f2063a949ad1dce5d0b8b2e7c630675d
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-11-26 12:10:59 +0100

    work done on more exercises

--- Tutorial/src/main.jsx
+++ Tutorial/src/main.jsx
@@ -3,14 +3,64 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-function MainSect() {
-  return <h1>Rendering a function</h1>
-}
+import { createElement } from 'react'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <h2>So App is being render from main.jsx</h2>
-    <MainSect />
   </StrictMode>,
 )
+
+
+
+
+// createRoot(document.getElementById('root')).render(
+//   <>
+//     <Page />
+//     {/* <MainPage /> */}
+//   </>
+// )
+
+
+// function MainPage() {
+//   return(
+//     <main>
+//       <li>I did this myself</li>
+//       <li>I did this myself</li>
+//       <li>I did this myself</li>
+//     </main>
+//   )
+// }
+
+// function Footer() {
+//   return(
+//     <footer>
+//       <small>This is a small footer text</small>
+//     </footer>
+//   )
+// }
+
+// function Page() {
+//   return(
+//     <>
+//       <header>
+//         <img src="./images/t1.png" alt="Image" />
+//       </header>
+//       <MainPage />
+//       <Footer />
+      
+//     </>
+//   )
+// }
+
+
+// const h1Element = document.createElement('h1');
+// h1Element.textContent = 'It is a happy day learning react';
+// h1Element.classList.add('header');
+// document.getElementById('root').appendChild(h1Element)
+
+// imperative: give specific step-by-step instructions
+// declarative: simply describe and let the program do his job.
+// a react component is a function that returns a react element.
