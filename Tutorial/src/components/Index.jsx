ad41e1066ac1347e4734a1cafa030e5feff0ebf7
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-11-28 15:12:16 +0100

    remove unused code

--- Tutorial/src/components/Index.jsx
+++
@@ -1,8 +0,0 @@
-// import { createRoot } from "react-dom/client";
-// import App from "../App.jsx";
-
-// const root = createRoot(document.getElementById('root'))
-
-// root.render(
-//     <App />
-// )
