b595a31ff5e03b9a8e0c5782c0ec8a4579580fe4
Okoeguale Godwin <okoegualegodwin@gmail.com>
2026-04-22 12:31:39 +0100

    finish building monster website

--- learnings/2026/monsters-rolodex/src/App.js
+++ learnings/2026/monsters-rolodex/src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>Monsters Rolodox</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={(e) => this.setState({ searchField: e.target.value })}
